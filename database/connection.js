0c7514908e7d856a9b9a87f061fde67eb70e3a97
Vitbacili <135022354+Vitbacili@users.noreply.github.com>
2024-08-29 08:50:00 -0300

    TCC

--- database/connection.js
+++ database/connection.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2/promise'); 
 
-const bd_usuario = 'us_aula_node'; // usuário
-const bd_senha = '123456'; // senha
+const bd_usuario = 'us_etim_122_g3'; // usuário
+const bd_senha = 'id0615'; // senha
 const bd_servidor = '10.67.22.216'; // servidor
 const bd_porta = '3306'; // porta
-const bd_banco = 's223_node'; // nome do banco
+const bd_banco = 'IronForce'; // nome do banco
 let connection;
     
 const config = {
